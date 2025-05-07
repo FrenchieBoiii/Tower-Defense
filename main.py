17bbd9e98e0fcd167f47077fb2b59f46e0185fe5
FrenchieBoiii <omar.bejarano1@gmail.com>
2025-05-07 15:39:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from algo import a_star
 from mapp import visualize_grid,create_map
 from tower import tower
+from Niveaux import Niveaux
+from Ennemi import Ennemi
+
+jeu = Niveaux("Map.xlsx","Wave.xlsx")
+dico_niveaux = jeu.création_des_niveaux()
 
 ennemis = []
 defenses = []
 
+argent = 1000
+vie = 100
+
 player_ready = False
 
-
+"""
 rows, cols = 20, 30
-money = 1000
-vie = 100
+
 coord_spawn = (rows/2,0)
 coord_arrivee = (rows/2,cols-1)
 
@@ -33,44 +40,53 @@ if path != []:
 else:
     print("No path found.")
 
+"""
+
 def acheter_tour():
     nouvelle_defense = Defense()
     defenses.append(nouvelle_defense)
         
 def debuter_jeu():
-    create_map()
-    derouler_jeu()
+    niveaux = Niveaux()
+    derouler_jeu(niveaux)
     
-def derouler_jeu():
-    niveau = 1
+def derouler_jeu(niveaux):
+    niveau = 0
+    
     while vie > 0:
-        derouler_tour(niveau)
+        mapp = niveaux[niveau].mapp
+        wave = niveaux[niveau].wave
+        derouler_tour(niveau, mapp, wave)
         niveau += 1
+        
     fin_de_partie()
 
-def phase_construction():
+def phase_construction(mapp):
     pass
     
+def spawn_ennemi(type_ennemi):
+    nouveau_ennemi = Ennemi(type_ennemi)
+    ennemis.append(nouveau_ennemi)
     
     
-def derouler_tour(niveau):
+def derouler_tour(niveau, mapp, wave):
     tic=0
     while not player_ready:
-        phase_construction()
+        phase_construction(mapp)
     while vie > 0 and ennemis != []:
-        derouler_tic(niveau,tic)
+        derouler_tic(niveau,tic, mapp, wave)
         tic+=1
         
-def derouler_tic(niveau,tic):
-    spawn_ennemis(wave[niveau][tic])
+def derouler_tic(niveau,tic, mapp, wave):
+    spawn_ennemi(wave[str(niveau+1)][tic])
     for ennemi in ennemis:
-        ennemi.update()
+        ennemi.deplacement()
     for defense in defenses:
         defense.update()
-    ennemis = [ennemi for ennemi in ennemis if (ennemi.vivant or ennemi.arrivee)]
+    ennemis[:] = [e for e in ennemis if e.vivant and not e.arrivee]
     
 def fin_de_partie():
-    pass
+    print("Vous n'avez plus de vie. Vous avez perdu.")
     
-
+    
     
