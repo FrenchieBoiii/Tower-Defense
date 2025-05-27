983ad776c836defec98985bad64692ba0bab12eb
FrenchieBoiii <omar.bejarano1@gmail.com>
2025-05-27 14:05:10 +0200

    Update Ennemi.py

--- Ennemi.py
+++ Ennemi.py
@@ -15,31 +15,44 @@ class Ennemi():
         self.deplacement = []
         self.distance_parcourue = 0
         self.type_ennemi = type_ennemi
-        self.degats = 10
         
         
-        if type_ennemi == 'n' :
-            self.hp = 10
+        if type_ennemi == 'paysan' :
+            self.hp = 30
+            self.degats = 1
+            self.recompense = 5
         
-        elif type_ennemi == 'b' :
-            self.hp = 20
+        elif type_ennemi == 'bandit' :
+            self.hp = 50
+            self.degats = 2
+            self.recompense = 8
+        elif type_ennemi == 'archer' :
+            self.hp = 40
+            self.degats = 2
+            self.recompense = 10
+        elif type_ennemi == 'chevalier' :
+            self.hp = 100
+            self.degats = 4
+            self.recompense = 15
+        elif type_ennemi == 'catapulte' :
+            self.hp = 80
+            self.degats = 10
+            self.recompense = 20
+        elif type_ennemi == 'seigneur' :
+            self.hp = 300
+            self.degats = 15
+            self.recompense = 50
+
             
     def damage(self,nb):
         self.hp -= nb
         if self.hp < 0 :
             self.vivant = False
-            """
-    def deplacer(self):
-        self.coord = self.deplacement.pop(0)
-        print(self.coord)
-        self.distance_parcourue += 1
-        if len(self.deplacement) == 0 :
-            #print("ennemi arrivee")
-            self.arrivee = True
-          """  
+
     def update(self):
         if self.hp <= 0:
             self.vivant = False
+            
         if self.deplacement:
             self.coord = self.deplacement.pop(0)
             self.distance_parcourue += 1
@@ -47,5 +60,3 @@ class Ennemi():
                 self.arrivee = True
         else:
             print("ennemi bloqué")
-
-        
