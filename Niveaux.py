a9681df1edbb1205bc150b82193defae9e66b212
JLienard2005 <juliette.lienard@orange.fr>
2025-05-28 23:16:41 +0200

    Niveaux.py

--- Niveaux.py
+++ Niveaux.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  2 14:42:45 2025
-
-@author: jroussel
-"""
 import openpyxl
 from mapp import Mapp
 
@@ -14,7 +8,13 @@ class Niveaux(): #classe qui a partir d'un tableau excel va générer des maps
         self.dico_mapps = self.création_des_mapps() #création du dico qui associe pour des numéros une map
         
         
-    def création_des_mapps(self): #pour chaque feuille du fichier, on va créer un objet map
+    def création_des_mapps(self): 
+        """
+        Fonction qui creéer un dictionnaire avec en cléle numéro de wave et en valeur une map qui correspnd à une feuille du fichier excel.
+    
+        sorties : 
+            dico : dictionnaire associant un numéro de wave avec une map
+        """
         wb = openpyxl.load_workbook(self.fichier_map)
         i = 0
         dico = {}
@@ -23,9 +23,21 @@ class Niveaux(): #classe qui a partir d'un tableau excel va générer des maps
             dico[i] = Mapp(self.fichier_map,sheet_name)                
         return dico
     
-    def affichage_niveau_test(self,nb): #fonction pour afficher une map dans l'invit de commande
+    def affichage_niveau_test(self,nb): 
+        """
+        Fonction qui affiche la carte associée à un niveau demandé
+        
+        Entrée :
+            nb : entier, numéro de wave
+        """
         return self.dico_mapps[nb].affichage_map()
     
-    def niveau(self,nb): #fonction pour récupérer
+    def niveau(self,nb): 
+        """
+        Fonction qui récupère la structure de la carte (mapp) pour le niveau demandé.
+        
+        Entrées :
+            nb : entier, numéro du niveau
+        """ 
         return self.dico_mapps[nb].mapp
                 
