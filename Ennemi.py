a204c235fbc7bfe651533a97b8a0f358a4a615f5
FrenchieBoiii <omar.bejarano1@gmail.com>
2025-05-31 17:35:03 +0200

    Update Ennemi.py

--- Ennemi.py
+++ Ennemi.py
@@ -1,3 +1,4 @@
+
 class Ennemi():
     
     def __init__(self,type_ennemi):
@@ -11,36 +12,37 @@ class Ennemi():
         
         
         if type_ennemi == 'paysan' :
-            self.hp = 30
+            self.hp = 40
             self.degats = 1
-            self.recompense = 5
+            self.recompense = 3
         
         elif type_ennemi == 'bandit' :
-            self.hp = 50
+            self.hp = 70
             self.degats = 2
-            self.recompense = 8
+            self.recompense = 6
         elif type_ennemi == 'archer' :
-            self.hp = 40
-            self.degats = 2
-            self.recompense = 10
-        elif type_ennemi == 'chevalier' :
-            self.hp = 100
+            self.hp = 50
             self.degats = 4
-            self.recompense = 15
+            self.recompense = 8
+        elif type_ennemi == 'chevalier' :
+            self.hp = 150
+            self.degats = 6
+            self.recompense = 10
         elif type_ennemi == 'catapulte' :
-            self.hp = 80
-            self.degats = 10
-            self.recompense = 20
+            self.hp = 120
+            self.degats = 12
+            self.recompense = 15
         elif type_ennemi == 'seigneur' :
-            self.hp = 300
-            self.degats = 15
+            self.hp = 500
+            self.degats = 40
             self.recompense = 50
 
             
     def damage(self,nb):
         """
-        fonction qui enlève à chaque ennemie les points de dégat et met à jour la variable vivant de l'ennemi'
-            Entrees: nb, nombre de dégats, int
+        fonction qui enlève à chaque ennemie les points de dégat et met à jour la variable vivant de l'ennemi
+            Entrées: 
+                nb: int qui indique les dégats reçus
         """    
         self.hp -= nb
         if self.hp < 0 :
@@ -48,7 +50,7 @@ class Ennemi():
 
     def update(self):
         """
-        fonction qui déplace les ennemies sur la map et met à jour leurs positions 
+        fonction qui déplace les ennemies sur la map, met à jour leurs positions et détecte s'ils sont arrivés
         """  
         if self.hp <= 0:
             self.vivant = False
