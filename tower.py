c22a0dde93221a90d0c579453dce6b6892e98325
FrenchieBoiii <omar.bejarano1@gmail.com>
2025-05-31 17:37:16 +0200

    Update tower.py

--- tower.py
+++ tower.py
@@ -1,6 +1,4 @@
-import algo
 import math
-
 
 class Defense:
     def __init__(self,position,prix):
@@ -19,33 +17,45 @@ class Tour(Defense):
 
     def update(self, ennemis, dt):
         """
-        Fonction qui tir en fonction du temps écoulé
+        Fonction qui appelle la fonction tirer à une cadence voulue
         
         Entrées :
-            ennemis : liste d'objets ennemi qui prennent des tirs
+            ennemis : liste d'ennemis qui prennent des tirs
             dt : temps qui s'est écoulé depuis le dernier appel de la fonction update
         """
         self.timer += dt
-        intervalle_tir = 1 / self.cadence
-        while self.timer >= intervalle_tir:
+        while self.timer >= self.cadence:
             self.tirer(ennemis)
-            self.timer -= intervalle_tir
+            self.timer -= self.cadence
+            self.cibles = []
         
         
     def tirer(self, ennemis):
+        """
+        Fonction qui enlève de la vie à certains ennemis dans la portée de la tour
+        
+        Entrées :
+            ennemis : liste d'ennemis qui prennent des tirs
+        """
         cibles = []
         for ennemi in ennemis:
             if self.dans_portee(ennemi.coord):
                 cibles.append(ennemi)
         
-        #cibles = sorted(cibles, key=lambda e: e.distance_parcourue)
-
-        
         nb_ennemi_a_tuer = min(self.nb_ennemis_touches, len(cibles))
         for i in range(nb_ennemi_a_tuer):
             cibles[i].hp -= self.degats
+        
             
     def dans_portee(self, pos_ennemi):
+        """
+        Fonction qui détecte si un ennemi est dans la portée de la tour
+        
+        Entrées :
+            pos_ennemi : tuple indiquant la position d'un ennemi
+        Sorties:
+            dans_portee : booléen qui indique si l'ennemi est dans la portée de la tour
+        """
         dx = pos_ennemi[0] - self.position[0]
         dy = pos_ennemi[1] - self.position[1]
         distance = math.sqrt(dx ** 2 + dy ** 2)
@@ -55,24 +65,24 @@ class Tour(Defense):
 
 class archer(Tour):
     def __init__(self, position):
-        super().__init__(position, prix=25, cadence=0.7, degats=15, distance_tir=3, nb_ennemis_touches=1)
+        super().__init__(position, prix=25, cadence=1, degats=10, distance_tir=3, nb_ennemis_touches=1)
 
 
 class mage(Tour):
     def __init__(self, position):
-        super().__init__(position, prix=50, cadence=1, degats=30, distance_tir=4, nb_ennemis_touches=2)
+        super().__init__(position, prix=50, cadence=1.5, degats=25, distance_tir=4, nb_ennemis_touches=1)
 
 
 class baliste(Tour):
     def __init__(self, position):
-        super().__init__(position, prix=75, cadence=2.5, degats=60, distance_tir=5, nb_ennemis_touches=3)
+        super().__init__(position, prix=80, cadence=3.5, degats=40, distance_tir=4, nb_ennemis_touches=2)
 
 class feu(Tour):
     def __init__(self, position):
-        super().__init__(position, prix=60, cadence=1, degats=30, distance_tir=3, nb_ennemis_touches=2)
+        super().__init__(position, prix=60, cadence=1.5, degats=30, distance_tir=3, nb_ennemis_touches=2)
 
 
-class Mur(Defense):
+class muraille(Defense):
     def __init__(self, position):
         super().__init__(position, prix=10)
     def update(self, ennemis, dt):
